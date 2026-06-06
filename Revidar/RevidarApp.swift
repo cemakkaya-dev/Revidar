33857d66fb09d1ea09c81441350e2256f006a2a5
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-06-06 18:01:32 -0300

    Initial Commit

---
+++ Revidar/RevidarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevidarApp.swift
+//  Revidar
+//
+//  Created by Cem Akkaya on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RevidarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
